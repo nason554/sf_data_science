f769dd959527f2c064243ce4d30f3cc733d88726
nason554 <nasonovan971@gmail.com>
2022-01-16 18:37:34 +0300

    v1

--- game_v2.py
+++ game_v2.py
@@ -16,11 +16,11 @@ def random_predict(number:int=1) -> int:
     count = 0
 
     while True:
-    		count += 1
-    		predict_number = np.random.randint(1, 101) # предполагаемое число
-    		if number == predict_number:
-        		break # выход из цикла, если угадали
-    return(count)
+        count += 1
+        predict_number = np.random.randint(1, 101) # предполагаемое число
+        if number == predict_number:
+            break # выход из цикла, если угадали
+        return(count)
 
 
 
